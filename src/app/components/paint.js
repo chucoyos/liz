4e533e9371c1a0be37d2951322af5f1f1eee65bd
chucoyos <unodev@outlook.com>
2024-01-19 00:21:03 -0600

    Add nav component

---
+++ src/app/components/paint.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+const Paint = () => {
+	return (
+		<div>
+			<Image
+				src='/belleza-efimera.jpg'
+				alt='Belleza Efimera'
+				width={250}
+				height={250}
+			/>
+		</div>
+	)
+}
+
+export default Paint
--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,15 @@
-import Image from 'next/image'
+import Nav from './components/nav'
+import Paint from './components/paint'
 
 export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col'>
+			<Nav />
 			<div className='mb-8 grid text-center lg:w-full pt-4'>
-				<h1 class='text-xl font-bold text-center'>Liz Gallery</h1>
+				<h1 class='text-xl font-bold text-center'>Gallery</h1>
 			</div>
 			<div class='flex flex-col items-center'>
-				<div>
-					<Image
-						src='/belleza-efimera.jpg'
-						alt='Belleza Efimera'
-						width={250}
-						height={250}
-					/>
-				</div>
+				<Paint />
 			</div>
 		</main>
 	)
